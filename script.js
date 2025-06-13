222fc0a568a1cfc8ada33e02623aeaeccf5ca77a
AchhyutB <achhyut.baral03@gmail.com>
2025-06-13 16:56:32 +0545

    script for eye button, hover animation and delete function in manage book section

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// password eye button
 function togglePassword() {
   const password = document.getElementById("password");
   const icon = document.getElementById("eye-icon");
@@ -12,3 +13,39 @@ function togglePassword() {
     icon.classList.add("fa-eye-slash");
   }
 }
+function toggleConfirmPassword() {
+  const input = document.getElementById("confirmPassword");
+  const icon = document.getElementById("eye-icon-confirm");
+  if (input.type === "password") {
+    input.type = "text";
+    icon.classList.replace("fa-eye-slash", "fa-eye");
+  } else {
+    input.type = "password";
+    icon.classList.replace("fa-eye", "fa-eye-slash");
+  }
+}
+
+// To do list (button click)
+const menuItems = document.querySelectorAll(".button_upload");
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    // Remove active classes from all
+    menuItems.forEach((i) => {
+      i.classList.remove("bg-[#88965E80]", "border", "border-[#88965E]");
+    });
+
+    // Add active classes to clicked
+    item.classList.add("bg-[#88965E80]", "border", "border-[#88965E]");
+  });
+});
+
+// delete from the managing books table
+const deleteButtons = document.querySelectorAll(".delete");
+deleteButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    button.closest("tr").remove();
+  });
+});
+
+//clicking edit change into (make-change and cancel) removing the (edit and delete button)
